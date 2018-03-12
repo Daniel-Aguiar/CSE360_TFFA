b63ad414095e6dff274dd8c93603646e220a7310
CraigKnoblauch <craigknoblauch@gmail.com>
2018-03-12 12:48:33 -0700

    Started working through how Controller should function

--- team-1-tffa/src/controller/Controller.java
+++ team-1-tffa/src/controller/Controller.java
@@ -1,5 +1,21 @@
 package controller;
 
-public class Controller {
+import common.Capsule;
 
+public class Controller {
+	
+	/* Members */
+	// Should I have a private Capsule, or should GUI have control over the Capsule?
+
+    // NOTE: Singleton implementation needed
+
+
+    // To be called by GUI
+    public Capsule goFormat() {		
+    		/* 
+    		 * Should make a call to Formatter, providing the path objects and the Statistics object
+    		 * Then should call Analyzer, providing it the Statistics Object
+    		 * Then should return the private Capsule object
+    		 */
+    }
 }
