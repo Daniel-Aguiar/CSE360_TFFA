744a5f839bbf8f5f1604966d094a893bcd78f861
CraigKnoblauch <craigknoblauch@gmail.com>
2018-03-13 11:35:24 -0700

    Updated comment for goFormat()

--- team-1-tffa/src/controller/Controller.java
+++ team-1-tffa/src/controller/Controller.java
@@ -3,17 +3,20 @@ package controller;
 import common.Capsule;
 
 public class Controller {
-	
+
 	/* Members */
+
+
 	// Should I have a private Capsule, or should GUI have control over the Capsule?
 
     // NOTE: Singleton implementation needed
 
 
     // To be called by GUI
-    public Capsule goFormat() {		
-    		/* 
+    public Capsule goFormat() {
+    		/*
     		 * Should make a call to Formatter, providing the path objects and the Statistics object
+			 * Before format(), use options object to set max line length to 80
     		 * Then should call Analyzer, providing it the Statistics Object
     		 * Then should return the private Capsule object
     		 */
