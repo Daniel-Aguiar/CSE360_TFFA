e2d9b73d66129eecec29638aea084c6fd4f2fa62
CraigKnoblauch <craigknoblauch@gmail.com>
2018-03-13 11:35:06 -0700

    Added maxLineLength

--- team-1-tffa/src/common/Options.java
+++ team-1-tffa/src/common/Options.java
@@ -3,6 +3,7 @@ package common;
 public class Options {
 
 	private String justy;
+	private int maxLineLength;
 
 	//No-arg constructor
 	public Options() {
@@ -14,9 +15,17 @@ public class Options {
 		this.justy = justy;
 	}
 
+	public void setMaxLineLength(int linelength) {
+		this.maxLineLength = linelength;
+	}
+
 	/* Getters */
 	public String getJusty() {
 		return this.justy;
+	}
+
+	public int getMaxLineLength() {
+		return this.maxLineLength;
 	}
 
 }//end Options class
