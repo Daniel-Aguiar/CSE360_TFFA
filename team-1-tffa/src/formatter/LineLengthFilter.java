1cc9dc01ab171dbb649a53e9dd48b71093ecdfa7
Lance <ltokuno@asu.edu>
2018-03-14 18:42:23 -0700

    Finished LineLengthFilter

--- team-1-tffa/src/formatter/LineLengthFilter.java
+++ team-1-tffa/src/formatter/LineLengthFilter.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.stream.Stream;
 
+
+/**
+ * This class assumes the input file is one long string with no newlines and a single space between words.
+ */
 public class LineLengthFilter extends FormatFilter {
 
 	
@@ -14,25 +18,126 @@ public class LineLengthFilter extends FormatFilter {
 
 	@Override
 	public void format() {
+			
+		int lineLen = params.getOpts().getMaxLineLength();
+		int end = lineLen;
+		int start = 0;
 		
-		try (Stream<String> lines = Files.lines(params.getInFile())) {
+		try {
+			byte[] bytes = Files.readAllBytes(params.getInFile());
 			
-			//get X # of chars
-			//rewind to the previous space
-			//exchange the space with a \n
-			//write line to file
+			//iterate through the array MaxLineLen elements at a time
+			while (end < bytes.length) {
+				
+				end = replacePrevSpace(bytes, end);
+				
+				if(end < 0) { //it found a word that was 80 characters or longer
+					end = replaceNextSpace(bytes, start + lineLen);
+				}
+				
+				start = end;
+				end += lineLen;
+			}//end while
 			
-		}catch (IOException e) {
-			  e.printStackTrace();
-		}
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}//end try-catch
 		
 	}//end format()
+	
+	
+	/**
+	 * Finds the next space before end, and replaces it with a '\n'
+	 * @param A
+	 * The array to check
+	 * @param end
+	 * the index to start checking
+	 * @return
+	 * the index of the previous space or -1 if a '\n' was encountered first.
+	 */
+	private int replacePrevSpace(byte[] A, int end) {
+		
+		//find the previous space or '\n'
+		while(end > 0 && A[end] != ' ' && A[end] != '\n') {
+			end -= 1;
+		}
+		
+		if(A[end] != '\n' || end <= 0) {
+			end = -1;
+		} else {
+			A[end] = '\n';
+		}
+		return end;
+	}//end replacePrevSpace()
 
 	
-	
-	private char[] readChars() {
-		return null;
-	}
+	/**
+	 * Finds the next space before end, and replaces it with a '\n'
+	 * @param A
+	 * The array to check
+	 * @param idx
+	 * the index to start checking
+	 * @return
+	 * the index of the previous space or -1 if a '\n' was encountered first.
+	 */
+	private int replaceNextSpace(byte[] A, int idx) {
+		//find the next space or '\n'
+		while(idx < A.length && A[idx] != ' ') {
+			idx += 1;
+		}
+		
+		//Check to see if we fell off the end of the array
+		if(idx >= A.length) {
+			idx = -1;
+		} else {
+			A[idx] = '\n';
+		}
+		return idx;
+	}//end replaceNextSpace()
 	
 	
 }//end class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
