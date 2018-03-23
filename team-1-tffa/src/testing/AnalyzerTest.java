dd649a0478f8b95b2c70a2fb5f42c37c6759b33e
CraigKnoblauch <craigknoblauch@gmail.com>
2018-03-23 11:38:30 -0700

    trying to implement JUnit; need mock objects

---
+++ team-1-tffa/src/testing/AnalyzerTest.java
@@ -0,0 +1,15 @@
+package testing;
+
+import analyzer.Analyzer;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AnalyzerTest {
+
+	@Test
+	public void countLinesTest() {
+		Analyzer anlyz = new Analyzer()
+	}
+
+}
